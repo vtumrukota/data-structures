8fcf4ae901f682acd867b80adf59bbf8f50f02c3
vtumrukota <vtumrukota@gmail.com>
2015-03-26 16:22:22 -0700

    Finished Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,51 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    //if empty
+    if(list.head === null){
+      list.head = newNode;
+      list.tail = newNode;
+    }
+    //if not empty
+    else{
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function(){
+
+    var currentHead = list.head;
+    //if one value
+    if(list.head === list.tail){
+      list.head = null;
+      list.tail = null;
+    }
+    //if multiple values
+    else{
+      list.head = list.head.next;
+    }
+    return currentHead.value;
   };
 
   list.contains = function(target){
+    //make an iterator variable that starts at head
+      var iterator = list.head;
+    //while iterator doesn't equal the tail
+    while(iterator !== null){
+      //check node that iterator points to
+      if(target === iterator.value){
+        //if matches target return true
+        return true;
+      }
+        //else set iterator to equal iterator.next
+      else{
+        iterator = iterator.next;
+      }
+    }
+    return false;
+
   };
 
   return list;

fdaf50faba7f395e4ec76bf36c81b3195aa0ca13
vtumrukota <vtumrukota@gmail.com>
2015-03-25 19:49:46 -0700

    Cleaned Up

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,8 +1,6 @@
 var Stack = function() {
   this.storage = {};
   this.stackSize = 0;
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
 };
 
 

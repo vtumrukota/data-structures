87dc847c9f9a9e34affe8dd1e9998c1796bde2b9
vtumrukota <vtumrukota@gmail.com>
2015-03-27 11:34:40 -0700

    Added Node Addition/Contains/Removal

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,15 +1,29 @@
 
 
 var Graph = function(){
+  //var this = Object.create(Graph.prototype)
+  this.nodes = {};
+  //return this;
 };
 
 Graph.prototype.addNode = function(node){
+  var graphNode = function(){
+    this.value = node;
+    this.linkedNodes ={};
+    // var newGraphNode ={};
+    // newGraphNode.value = value;
+    // newGraphNode.linkedNodes = {};
+    // return newGraphNode;
+  }
+ this.nodes[node] = new graphNode(node);
 };
 
 Graph.prototype.contains = function(node){
+  return this.nodes.hasOwnProperty(node);
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.nodes[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){

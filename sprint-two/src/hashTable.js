3a2a716af2ffb9e7024ef89c93241ce6bc1aed18
vtumrukota <vtumrukota@gmail.com>
2015-03-28 15:49:47 -0700

    Completed HashTable EC and refactored code

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,9 +2,6 @@ var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
   this._size = 0;
-  // this._storage.each(function(bucket , i ,storage){
-  //   storage[i] = [];
-  // });
   for(var i = 0; i < this._limit; i++){
     this._storage.set(i,[]);
   }
@@ -18,8 +15,10 @@ HashTable.prototype.insert = function(k, v){
   this._storage.set(i , existingBucket);
   this._size++;
 
+  //resize if needed
   if(this._size >= .75 * this._limit){
-    this.increaseSize();
+    this._limit = this._limit * 2;
+    this._storage = this.reHash(this._storage,this._limit * .5, this._limit);
   }
 };
 
@@ -43,52 +42,39 @@ HashTable.prototype.remove = function(k){
   var retrievedValue = this._storage.get(i);
   if(retrievedValue === null || retrievedValue === undefined){
     return;
-  } else { 
-    if(retrievedValue.length > 0){
-      var indexTuple;
-      _.each(retrievedValue, function(tuple,eachIndex){
-        if(tuple[0] === k){
-          indexTuple = eachIndex;
-        }
-      });
-      retrievedValue.splice(indexTuple,1);
-      this._storage.set(i,retrievedValue);
-    } else {
-      this.storage.set(i , []);
-    }
-    this._size--;
-    if(this._size < .25 * this._limit){
-      this.decreaseSize();
-    }
   }
-};
+  if(retrievedValue.length > 0){
+    var indexTuple;
+    _.each(retrievedValue, function(tuple,eachIndex){
+      if(tuple[0] === k){
+        indexTuple = eachIndex;
+      }
+    });
+    retrievedValue.splice(indexTuple,1);
+    this._storage.set(i,retrievedValue);
+  } else {
+      this.storage.set(i , []);
+  }
+  this._size--;
 
-
-// GET current size limit
-// CREATE new Array Like Object with size limit * 2
-// SET each element to empty array
-// FOR each element in old storage
-//   RETRIEVE element and rehash with new size limit
-//   INSERT element into new storage object
-// SET old storage object to new storage object
-
-HashTable.prototype.increaseSize = function(){
-  this._limit = this._limit * 2;
-  this._storage = this.reHash(this._storage,this._limit * .5, this._limit);
-};
-
-HashTable.prototype.decreaseSize = function(){
-  this._limit = this._limit * .5;
-  this._storage = this.reHash(this._storage,this._limit*2,this._limit);
+  //resize if needed
+  if(this._size < .25 * this._limit){
+    this._limit = this._limit / 2;
+    this._storage = this.reHash(this._storage,this._limit * 2, this._limit);
+  }
 };
 
 HashTable.prototype.reHash = function(fromArray,fromSize,toSize){
+  //set new Hash Table Size
   var newStorage = LimitedArray(toSize);
+  //Create empty array inside each index
   for(var i = 0; i < toSize; i++){
     newStorage.set(i,[]);
   }
+  //pull out each bucket from previous Hash Table
   for(var i = 0; i < fromSize; i++){
     var retrievedBucket = fromArray.get(i);
+    //Store old Tuples into new Hash table w/ new Hash values
     for(var j = 0; j < retrievedBucket.length; j++){
       var hashIndex = getIndexBelowMaxForKey(retrievedBucket[j][0],toSize);
       var newStorageRetrievedBucket = newStorage.get(hashIndex);

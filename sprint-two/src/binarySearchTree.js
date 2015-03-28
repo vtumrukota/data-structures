89c8d5426d5691ed5f1c2ba51a75813d9c58238b
vtumrukota <vtumrukota@gmail.com>
2015-03-27 20:55:13 -0700

    Finished binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -35,10 +35,38 @@ BinarySearchTree.prototype.insert = function(value){
 };
 
 BinarySearchTree.prototype.contains = function(target){
+  var hasTarget = false;
 
+  var recursiveCheck = function(head){
+    if(head === null){
+      return;
+    }
+    if(head.value === target){
+      hasTarget = true;
+    }else{
+      if(target > head.value){
+        recursiveCheck(head.right);
+      }else{
+        recursiveCheck(head.left);
+      }
+    }
+  };
+  recursiveCheck(this);
+  return hasTarget;
 };
 
-BinarySearchTree.prototype.depthFirstLog = function(cb){};
+BinarySearchTree.prototype.depthFirstLog = function(cb){
+  var recursiveCall = function(head){
+    cb(head.value);
+    if(head.left !== null){
+      recursiveCall(head.left);
+    }
+    if(head.right !== null){
+      recursiveCall(head.right);
+    }
+  }
+  recursiveCall(this);
+};
 
 
 
